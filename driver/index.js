883f724381c19bf06a18ec8e3707841a62d0d41d
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-05-31 11:12:52 -0700

    code review lab10

--- driver/index.js
+++ driver/index.js
@@ -1,23 +1,6 @@
 'use strict';
 
-let eventEmitter = require('../eventEmitter');
+const eventEmitter = require('../eventEmitter');
+const { handlePickupAndDelivery } = require('./handler');
 
-const pickupHandler = (payload) => {
-  setTimeout(() => {
-    console.log('DRIVER: picked up ', payload.orderId);
-    
-    eventEmitter.emit('EVENT', 'in-transit', payload);
-    eventEmitter.emit('IN-TRANSIT', payload);
-  }, 500);
-};
-
-const intransitHandler = (payload) => {
-  setTimeout(() => {
-    console.log('DRIVER: delivered up', payload.orderId);
-    console.log('VENDOR: Thank you for delivering', payload.orderId);
-    
-    eventEmitter.emit('EVENT', 'delivered', payload);
-  }, 500);
-};
-  
-module.exports = { pickupHandler, intransitHandler };
+eventEmitter.on('pickup', handlePickupAndDelivery); 
--- vendor/index.js
+++ vendor/index.js
@@ -1,19 +1,11 @@
 'use strict';
 
+const { orderHandler, deliveredMessage }= require('./handler');
 const eventEmitter = require('../eventEmitter');
 
-const newOrderHandler = (payload) => {
-  setTimeout(() => {
-    eventEmitter.emit('EVENT', 'pickup', payload);
-    eventEmitter.emit('PICKUP', payload);
-  }, 5000);
-};
+// starts the event cycle, note that the pickup emit is inside the orderHandler
+setInterval(() => {
+  orderHandler();
+}, 5000);
 
-const deliveredHandler = (payload) => {
-    setTimeout(() => {
-        console.log('VENDOR: Thank you for your order', payload.customer);
-        eventEmitter.emit('DELIVERED', payload);
-      }, 500);
-}
-
-module.exports = { newOrderHandler, deliveredHandler };
+eventEmitter.on('delivered', deliveredMessage);
